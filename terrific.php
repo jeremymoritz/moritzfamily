22a3748dbd65cede2e7c4587ba699b5e5e89c7f6
Jeremy Moritz <jeremy@jeremymoritz.com>
2021-11-28 13:15:30 -0600

    feat: update Little Miss Terrific to use Bootstrap 5

--- terrific.php
+++ terrific.php
@@ -7,20 +7,17 @@ require_once('inc/mf.php');
 <body>
 <?=$topnav;?>
 <?=$topper;?>
-  <section class='main' id='terrific'>
+  <section class='main px-2 px-md-4' id='terrific'>
     <h1><?=$title;?></h1>
     <section>
       <h2>Little Miss Terrific</h2>
-      <noscript>
-        <p id='warning'>WARNING: JavaScript must be enabled on your browser to view this story! Your browser is currently not configured to display JavaScript.  Please <a href='http://jeremyandchristine.com/_old/_oldpages/terrific-old.php'>Click Here</a> to view the story in another format.</p>
-      </noscript>
-      <table id='lmttable'>
-        <tr>
-          <td class='btn'><img src='img/lmt/backbtn.gif' alt='Back' id='backbtn' class='mouseover'></td>
-          <td><img src='img/lmt/lmt1.jpg' alt='1' id='lmtpage'></td>
-          <td class='btn'><img src='img/lmt/fwdbtn.gif' alt='Forward' id='fwdbtn' class='mouseover'></td>
-        </tr>
-      </table>
+      <div id='lmttable'>
+        <button id='backbtn' class="btn btn-primary"><i class="bi-arrow-left"></i></button>
+        <button id='fwdbtn' class="btn btn-primary"><i class="bi-arrow-right"></i></button>
+        <div class="main-image">
+          <img src='img/lmt/lmt1.jpg' alt='1' id='lmtpage' class="img-fluid">
+        </div>
+      </div>
     </section>
   </section>
 <?=$footer;?>
