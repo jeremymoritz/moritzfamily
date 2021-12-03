0524f02e976e18fce6e312ce0b86b150439e12e6
Jeremy Moritz <jeremy@jeremymoritz.com>
2021-12-03 09:52:19 -0600

    Update mission statement and highlight reel

--- mission.php
+++ mission.php
@@ -1,4 +1,4 @@
-<?php	//	Jeremy And Christine Moritz (MoritzFamily.com)
+<?php //  Jeremy And Christine Moritz (MoritzFamily.com)
 $title = "The Moritz Family Mission Statement";
 require_once('inc/mf.php');
 ?>
@@ -12,7 +12,14 @@ require_once('inc/mf.php');
       <h2><span class="d-none d-sm-inline">The </span>
         Moritz Family<br class="d-lg-none">
         Mission Statement</h2>
-      <p>We, Jeremy and Christine Moritz, have high expectations for our family.  We intend to honor God, live by our principles, love and respect each other for the rest of our lives.  To reach these objectives, we strive to keep the following priorities at the forefront of our minds and make every reasonable effort to live in line with these principles at all times.</p>
+      <p>
+        We, Jeremy and Christine Moritz, have high expectations for our family.
+        We intend to honor God, live by our principles, love and respect each
+        other for the rest of our lives.  To reach these objectives, we strive
+        to keep the following priorities at the forefront of our minds and make
+        every reasonable effort to live in line with these principles at all
+        times.
+      </p>
       <ol>
         <li>
           <strong>God is #1 in our family.</strong>  We make all of our
@@ -22,11 +29,11 @@ require_once('inc/mf.php');
         <li>
           <strong>After our commitment to the Lord, our marriage is above all
           else.</strong>  This relationship takes priority over any and all
-          other obligations, including, but not limited to: children, work,
-          ministry, family, hobbies/interests, and recreation.  We continually
-          provide love, support, security, and companionship for each other and
-          cultivate this relationship through regular time alone together.
-          (Ephesians 5:33)
+          other obligations and relationships, including, but not limited to:
+          children, work, ministry, family, hobbies/interests, and recreation.
+          We continually provide love, support, security, and companionship for
+          each other and cultivate this relationship through regular time alone
+          together. (Ephesians 5:33)
         </li>
         <li>
           <strong>We make a great effort to show love and support for our
@@ -36,10 +43,14 @@ require_once('inc/mf.php');
           to guide our parenting. (Proverbs 22:6)
         </li>
         <li>
-          <strong>All big decisions are made together.</strong>  We always pray
-          about big decisions and include each other in the process.  When our
-          children are old enough to understand these decisions, they will have
-          a prominent voice as well. (Romans 15:5-6)
+          <strong>In this house, we are friends.</strong>  Our children treat
+          one another with affection and respect. They advocate for each other's
+          success and well-being.  They speak kindly to and about one another,
+          even in each other's absence. (Matthew 7:12)
+        </li>
+        <li>
+          <strong>All big decisions are made together.</strong>  We pray about
+          big decisions and include each other in the process. (Romans 15:5-6)
         </li>
         <li>
           <strong>We manage money wisely.</strong>  We invest, save, give
@@ -51,27 +62,20 @@ require_once('inc/mf.php');
           other's pursuits and hobbies even when they do not match our own.
           (Philippians 2:4)
         </li>
-        <!-- <li>
-          <strong>We tithe at least 10% of our income to God's service,</strong>
-          regardless of how much or little that may be.  As soon as our children
-          are old enough to receive and spend money, they too will be taught the
-          values and principles of tithing. (Malachi 3:10)
-        </li> -->
         <li>
           <strong>We monitor our entertainment.</strong>  We take careful note
-          of the television shows, movies, music, events, websites, and
+          of the television shows, movies, music, events, websites, apps, and
           activities with which we entertain ourselves, and we avoid any that
-          may be detrimental to our thoughts or are out of line with our mission
-          statement.  We are also careful to temper the amount time we spend
-          with any entertainment medium. (Psalm 101:3)
+          we deem to be detrimental to our thoughts or are out of line with our
+          mission statement.  We are also careful to temper the amount time we
+          spend with any entertainment medium. (Psalm 101:3)
         </li>
         <li>
           <strong>We choose our friends carefully.</strong>  All of our closest
           friendships are those that serve to support and strengthen our
           spiritual, marital, and family commitments.  We refrain from
-          developing intimate friendships with anyone who may encourage us to
-          act out of line with our priorities or our mission. (Proverbs
-          13:20)
+          developing intimate friendships with anyone who would encourage us to
+          act out of line with our priorities or our mission. (Proverbs 13:20)
         </li>
         <li>
           <strong>All of our actions are characterized by honesty and
@@ -80,12 +84,14 @@ require_once('inc/mf.php');
         </li>
       </ol>
       <p>This is our family's mission.  We will keep this statement in plain
-        view with the understanding that, although we may often stray from the
-        path we intend to follow, this Mission Statement will serve as a
+        view with the understanding that, although we may sometimes stray from
+        the path we intend to follow, this Mission Statement will serve as a
         reminder and a standard to which we will continually return.</p>
       <p>Renewed and updated January 1, <?=date('Y');?>.</p>
       <p>~The Moritz Family<br>
-        Jeremy &amp; Christine, Angel, Tony, Harmony, Charity, Chase, and Symphony</p>
+        Jeremy &amp; Christine, Angel, Tony, Harmony, Charity, Chase, and
+        Symphony
+      </p>
     </section>
   </section>
 <?=$footer;?>
